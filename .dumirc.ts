1f90ce645df4d4f71018cf935cdf653855b9223e
zijin <zijin345@126.com>
2023-11-16 16:41:23 +0800

    feat: 测试

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,6 +3,4 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: { name: 'sic-ui' },
-  title: 'SIC-UI',
-  logo: './src/assets/logo.svg',
 });
