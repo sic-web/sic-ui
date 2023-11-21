29c6192a74e1e4784916f3adc79e387ddf021522
zijin <zijin345@126.com>
2023-11-21 20:33:59 +0800

    feat: 滚动

--- src/AnchorUI/demo/user-anchor.tsx
+++ src/AnchorUI/demo/user-anchor.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { AnchorUI } from 'sic-ui';
-export default () => (
-  <div style={{ display: 'flex' }}>
-    <AnchorUI />
-    <div style={{ flex: 1 }}>
-      <div style={{ height: '200px', backgroundColor: 'rgba(255,0,0,0.2)' }} />
-      <div style={{ height: '200px', backgroundColor: 'rgba(255,0,0,0.2)' }} />
-      <div style={{ height: '200px', backgroundColor: 'rgba(255,0,0,0.2)' }} />
+export default () => {
+  const ref: any = useRef(null);
+  return (
+    <div style={{ display: 'flex' }}>
+      <AnchorUI
+        getContainer={() => ref.current}
+        items={[
+          {
+            key: 'part-1',
+            href: '#part-1',
+            title: '主体资质',
+          },
+          {
+            key: 'part-2',
+            href: '#part-2',
+            title: '客户来源',
+          },
+          {
+            key: 'part-3',
+            href: '#part-3',
+            title: '项目信息',
+          },
+        ]}
+      />
+      <div style={{ flex: 1, overflow: 'scroll', height: 400 }} ref={ref}>
+        <div id="part-1" style={{ height: '200px', backgroundColor: 'rgba(255,0,0,0.2)' }} />
+        <div id="part-2" style={{ height: '200px', backgroundColor: 'rgba(0,255,0,0.2)' }} />
+        <div id="part-3" style={{ height: '200px', backgroundColor: 'rgba(0,0,255,0.2)' }} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
--- src/AnchorUI/index.tsx
+++ src/AnchorUI/index.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.scss';
 
 const AnchorUI = (props: any) => {
-  const {} = props;
-  return <div className="sic-anchorui">123</div>;
+  const { items, getContainer } = props;
+  const [current, setCurrent] = useState(0);
+
+  const click = (index: number, id: string) => {
+    setCurrent(index + 1);
+    console.log('---', id);
+  };
+
+  console.log('--getContainer', getContainer());
+  return (
+    <div className="sic-anchorui">
+      {items?.map((item: any, index: number) => {
+        return (
+          <div
+            className={`sic-anchorui-item ${current === index ? 'sic-anchorui-item-select' : null}`}
+            key={item.id}
+            onClick={() => click(index, item.id)}
+          >
+            {item.title}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default AnchorUI;
