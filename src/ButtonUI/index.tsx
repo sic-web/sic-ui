5b2a6af3371ba5920ba0e4e2ea05cf8ca1c4dd92
xiaoli0705 <xiaoli980705@gmail.com>
2023-11-24 14:02:02 +0800

    fix: bugfix

--- src/ButtonUI/index.tsx
+++ src/ButtonUI/index.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import { Button, ButtonProps } from 'antd';
 import './index.scss';
-
 interface ButtonUIProps extends Omit<ButtonProps, 'type'> {
-  type: 'default' | 'primary' | 'dashed' | 'link' | 'text' | 'border' | 'delete' | undefined;
+  type?: any;
   icon?: React.ReactNode;
   children?: React.ReactNode;
 }
 
 const ButtonUI = (props: ButtonUIProps) => {
   const { type, icon, children, ...otherProps } = props;
-  const getType = () => (type === 'border' || type === 'delete' ? 'default' : type);
-  const getClassName = () => {
-    const classNames = ['sic-buttonui'];
-    if (type === 'border') classNames.push('sic-buttonui-border');
-    if (type === 'delete') classNames.push('sic-buttonui-delete');
-    return classNames.join(' ');
-  };
-
   return (
-    <Button className={getClassName()} type={getType()} {...otherProps}>
+    <Button className="sic-buttonui" type={type} {...otherProps}>
       {children}
       {icon && <span className="sic-buttonui-icon">{icon}</span>}
     </Button>
