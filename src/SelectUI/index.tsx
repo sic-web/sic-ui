6d35930eadf7159aca6e40f981c900c9d307e5fd
xiaoli0705 <xiaoli980705@gmail.com>
2023-11-29 10:18:15 +0800

    fix: 样式修改

--- src/SelectUI/index.tsx
+++ src/SelectUI/index.tsx
@@ -2,28 +2,36 @@ import React from 'react';
 import { Select } from 'antd';
 import './index.scss';
 
-interface optionType {
-  extend: any;
-  label: string;
-  value: number;
-}
 /**
  * @description: 下拉框
  * @param {boolean} showSearch 是否显示搜索
  * @param {boolean} allowClear 是否显示清除
  * @param {Function} filterOption 过滤条件
  * @param {Function} filterSort 排序条件
+ * @param {Function} fieldNames 自定义字段
  * @return {*}
  */
+
 const SelectUI = (props: any) => {
-  const { options = [], showSearch = true, allowClear = true, filterOption, filterSort, onChange, style, ...otherProps } = props;
-  const defaultFilterOption = (input: string, option: optionType) => {
-    const label = option?.label.toString();
+  const {
+    options = [],
+    showSearch = true,
+    allowClear = true,
+    filterOption,
+    filterSort,
+    fieldNames,
+    onChange,
+    style,
+    ...otherProps
+  } = props;
+
+  const defaultFilterOption = (input: string, option: any) => {
+    const label = fieldNames?.label ? option?.[fieldNames?.label].toString() : option?.label.toString();
     return label.includes(input);
   };
-  const defaultFilterSort = (optionA: optionType, optionB: optionType) => {
-    const labelA = optionA?.label.toString().toLowerCase();
-    const labelB = optionB?.label.toString().toLowerCase();
+  const defaultFilterSort = (optionA: any, optionB: any) => {
+    const labelA = fieldNames?.label ? optionA?.[fieldNames?.label].toString().toLowerCase() : optionA?.label.toString().toLowerCase();
+    const labelB = fieldNames?.label ? optionB?.[fieldNames?.label].toString().toLowerCase() : optionB?.label.toString().toLowerCase();
     return labelA.localeCompare(labelB);
   };
 
@@ -37,6 +45,7 @@ const SelectUI = (props: any) => {
       optionFilterProp="label"
       filterOption={filterOption ?? defaultFilterOption}
       filterSort={filterSort ?? defaultFilterSort}
+      fieldNames={fieldNames}
       onChange={onChange}
       {...otherProps}
     ></Select>
