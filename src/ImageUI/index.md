d74b322c59716e4267b4b0a340364b22868240b0
xiaoli0705 <xiaoli980705@gmail.com>
2023-12-06 13:51:37 +0800

    fix: bugfix

---
+++ src/ImageUI/index.md
@@ -0,0 +1,55 @@
+---
+nav:
+  title: 组件
+  path: /components
+group:
+  title: 基础组件
+  order: 2
+---
+
+# Image-图片展示组件
+
+:::success{title=使用中}
+国信灵工后台管理系统-图片展示组件
+:::
+
+## 场景故事
+
+## 代码演示
+
+💎 基础使用
+
+```jsx
+import React from 'react';
+import { Space } from 'antd';
+import { ImageUI } from 'sic-ui';
+
+const App: React.FC = () => (
+  <Space>
+    <ImageUI src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" width={200} height={200} />
+    <ImageUI src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" width={200} height={200} maskType={2} />
+    <ImageUI src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" width={200} height={200} maskType={3} />
+  </Space>
+);
+export default App;
+```
+
+## API 使用文档
+
+ImageUI 引用 Ant Design 组件库，在使用过程中不做如何侵入性的更改
+
+<font size=1>
+
+|       属性       | 说明                   |        类型         | 默认值 | 是否必传 |
+| :--------------: | ---------------------- | :-----------------: | :----: | :------: |
+|       src        | 地址                   |       String        |   -    |    是    |
+|      width       | 宽度                   |       number        |   -    |    否    |
+|      height      | 长度                   |       number        |   -    |    否    |
+|     preview      | 是否可预览             |       Boolean       |  true  |    否    |
+|   placeholder    | 图片占位               |       Boolean       |  true  |    否    |
+|     maskType     | 蒙层类型               |       Number        |   1    |    否    |
+|    className     | 类名                   |       String        |   -    |    否    |
+|      style       | 样式                   | React.CSSProperties |   -    |    否    |
+| clickOtherButton | 点击蒙层右边按钮后回调 |      ()=>void       |   -    |    否    |
+
+</font>
---
+++ src/ImageUI/index.tsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import { Image, Space } from 'antd';
+import { IconUI } from 'sic-ui';
+import {
+  DownloadOutlined,
+  RotateLeftOutlined,
+  RotateRightOutlined,
+  SwapOutlined,
+  ZoomInOutlined,
+  ZoomOutOutlined,
+} from '@ant-design/icons';
+import './index.scss';
+interface ImageUIProps {
+  src: string;
+  width?: number;
+  height?: number;
+  preview?: boolean;
+  placeholder?: boolean;
+  maskType?: number;
+  className?: string;
+  style?: React.CSSProperties;
+  clickOtherButton?: () => void;
+}
+
+const ImageUI = (props: ImageUIProps) => {
+  const { src, width, height, preview = true, placeholder = true, maskType = 1, className, style, clickOtherButton, ...otherProps } = props;
+  const onDownload = () => {
+    fetch(src)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = URL.createObjectURL(new Blob([blob]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'image.png';
+        document.body.appendChild(link);
+        link.click();
+        URL.revokeObjectURL(url);
+        link.remove();
+      });
+  };
+  const clickRight = (e: any) => {
+    e.stopPropagation();
+    if (clickOtherButton) {
+      clickOtherButton();
+    }
+  };
+  //蒙层类型
+  const renderMask = () => {
+    switch (maskType) {
+      case 1:
+        return (
+          <div className="sicImageUI-mask">
+            <IconUI name="PreviewOpen" size="14" fill="#fff" />
+            <div className="sicImageUI-mask-name">预览</div>
+          </div>
+        );
+      case 2:
+        return (
+          <div className="sicImageUI-mask">
+            <div className="sicImageUI-mask-left">
+              <IconUI name="PreviewOpen" size="14" fill="#fff" />
+              <div className="sicImageUI-mask-name">预览</div>
+            </div>
+            <div className="sicImageUI-mask-right" onClick={clickRight}>
+              <IconUI name="Export" size="14" fill="#fff" />
+              <div className="sicImageUI-mask-name">上传</div>
+            </div>
+          </div>
+        );
+      case 3:
+        return (
+          <div className="sicImageUI-mask">
+            <div className="sicImageUI-mask-left">
+              <IconUI name="PreviewOpen" size="14" fill="#fff" />
+              <div className="sicImageUI-mask-name">预览</div>
+            </div>
+            <div className="sicImageUI-mask-right" onClick={clickRight}>
+              <IconUI name="Delete" size="14" fill="var(--red)" />
+              <div className="sicImageUI-mask-delete">删除</div>
+            </div>
+          </div>
+        );
+      default:
+        return (
+          <div className="sicImageUI-mask">
+            <IconUI name="PreviewOpen" size="14" fill="#fff" />
+            <div className="sicImageUI-mask-name">预览</div>
+          </div>
+        );
+    }
+  };
+  const toolbar = () => {
+    if (src && preview) {
+      return {
+        mask: renderMask(),
+        toolbarRender: (
+          _: any,
+          { transform: { scale }, actions: { onFlipY, onFlipX, onRotateLeft, onRotateRight, onZoomOut, onZoomIn } }: any,
+        ) => (
+          <Space size={12} className="sicImageUI-toolbar">
+            <DownloadOutlined onClick={onDownload} />
+            <SwapOutlined rotate={90} onClick={onFlipY} />
+            <SwapOutlined onClick={onFlipX} />
+            <RotateLeftOutlined onClick={onRotateLeft} />
+            <RotateRightOutlined onClick={onRotateRight} />
+            <ZoomOutOutlined disabled={scale === 1} onClick={onZoomOut} />
+            <ZoomInOutlined disabled={scale === 50} onClick={onZoomIn} />
+          </Space>
+        ),
+      };
+    } else {
+      return false;
+    }
+  };
+  return (
+    <div className={`sicImageUI ${className ?? ''}`} style={style}>
+      <Image src={src} width={width} height={height} preview={toolbar()} placeholder={placeholder} {...otherProps} />
+    </div>
+  );
+};
+
+export default ImageUI;
