8ff542b0d1b637a97ba73afdc5e0741e0f4f9614
zijin <zijin345@126.com>
2023-12-21 16:49:14 +0800

    feat: 更新

--- src/Tiga/index.tsx
+++ src/Tiga/index.tsx
@@ -51,7 +51,24 @@ const filterTableHeader = (columns: Columns[], tableHeader: TableHeaderItem[]) =
 };
 
 const Tiga = (props: any) => {
-  const { dataSource, initialColumns = [], morphColumns, tableHeader, ...otherProps } = props;
+  // tag morphColumns 用这个参数代替initialColumns传入所改变的列
+  const { dataSource, initialColumns = [], morphColumns = [], tableHeader, tableData, pageSize, total, current, ...otherProps } = props;
+  let t_dataSource = dataSource;
+  let t_pageSize = pageSize;
+  let t_current = current;
+  let t_total = total;
+
+  // 特殊字段-完全适配于A/B/OS项目
+  if (tableData) {
+    // 表格数据
+    t_dataSource = tableData?.records;
+    // 表格页码
+    t_pageSize = tableData?.size;
+    // 表格当前
+    t_current = tableData?.current;
+    // 表格总数
+    t_total = tableData?.total;
+  }
   // 新的列数据
   const [columns, setColumns] = useState<any>();
 
@@ -61,8 +78,8 @@ const Tiga = (props: any) => {
     setColumns(list);
   }, [tableHeader]);
   return (
-    <LoadingUI spinning={dataSource && columns ? false : true}>
-      <TableUI columns={columns} dataSource={dataSource} {...otherProps} />
+    <LoadingUI spinning={t_dataSource && columns ? false : true}>
+      <TableUI columns={columns} dataSource={t_dataSource} pageSize={t_pageSize} current={t_current} total={t_total} {...otherProps} />
     </LoadingUI>
   );
 };
