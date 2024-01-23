bba1e6c2a9e9612948e52ce8c0eccc37513b1759
xiaoli0705 <xiaoli980705@gmail.com>
2024-01-23 17:28:16 +0800

    fix: 优化时间组件

--- src/DateUI/index.tsx
+++ src/DateUI/index.tsx
@@ -1,36 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import { DatePicker, ConfigProvider } from 'antd';
-import zhCN from 'antd/locale/zh_CN';
 import dayjs from 'dayjs';
-import './index.scss';
+import zhCN from 'antd/locale/zh_CN';
+import 'dayjs/locale/zh-cn';
 
+dayjs.locale('zh-cn');
+import './index.scss';
 const DateUI = (props: any) => {
-  const { value, onChange, format = 'YYYY-MM-DD', ...otherProps } = props;
+  const { className, value, onChange, format = 'YYYY-MM-DD', rangePicker = true, ...otherProps } = props;
 
   const [realValue, setRealValue] = useState();
 
   useEffect(() => {
-    if (value && value?.[0] && value?.[1]) {
-      const newValue: any = [dayjs(value[0], format), dayjs(value[1], format)];
-      setRealValue(newValue);
+    if (rangePicker) {
+      if (value && value?.[0] && value?.[1]) {
+        const newValue: any = [dayjs(value[0], format), dayjs(value[1], format)];
+        setRealValue(newValue);
+      } else {
+        setRealValue(undefined);
+      }
     } else {
-      setRealValue(undefined);
+      if (value) {
+        const newValue: any = dayjs(value, format);
+        setRealValue(newValue);
+      } else {
+        setRealValue(undefined);
+      }
     }
   }, [value]);
 
   const changeDate = (e: any) => {
-    const newTime = e?.map((date: any) => date.format(format));
+    let newTime;
+    if (rangePicker) {
+      newTime = e?.map((date: any) => date.format(format));
+    } else {
+      newTime = e.format(format);
+    }
+
     setRealValue(e);
     onChange(newTime);
   };
 
   return (
     <ConfigProvider locale={zhCN}>
-      <div className="sic-dateui">
-        <DatePicker.RangePicker separator={'至'} value={realValue} onChange={changeDate} {...otherProps} />
-      </div>
+      {rangePicker ? (
+        <DatePicker.RangePicker
+          className={`sic-dateui ${className ?? ''}`}
+          separator={'至'}
+          value={realValue}
+          onChange={changeDate}
+          {...otherProps}
+        />
+      ) : (
+        <DatePicker className={`sic-dateui ${className ?? ''}`} value={realValue} onChange={changeDate} {...otherProps} />
+      )}
     </ConfigProvider>
   );
 };
-
 export default DateUI;
