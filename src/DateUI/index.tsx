ba6cc39234fce79dca39d9e6c22c83810e637160
xiaoli0705 <xiaoli980705@gmail.com>
2024-04-16 10:47:30 +0800

    fix: 优化筛选

--- src/DateUI/index.tsx
+++ src/DateUI/index.tsx
@@ -1,18 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { DatePicker, ConfigProvider, Select } from 'antd';
+import { DatePicker, ConfigProvider } from 'antd';
 import dayjs from 'dayjs';
-import quarterOfYear from 'dayjs/plugin/quarterOfYear';
 import zhCN from 'antd/locale/zh_CN';
 import 'dayjs/locale/zh-cn';
-import './index.scss';
 
-dayjs.extend(quarterOfYear);
+dayjs.locale('zh-cn');
+import './index.scss';
 const DateUI = (props: any) => {
-  const { className, value, onChange, format = 'YYYY-MM-DD', rangePicker = true, isShowSelect = false, ...otherProps } = props;
-  //日期选择框值
-  const [realValue, setRealValue] = useState<any>();
-  //下拉框值
-  const [selectValue, setSelectValue] = useState<any>();
+  const { className, value, onChange, format = 'YYYY-MM-DD', rangePicker = true, ...otherProps } = props;
+
+  const [realValue, setRealValue] = useState();
 
   useEffect(() => {
     if (rangePicker) {
@@ -21,7 +18,6 @@ const DateUI = (props: any) => {
         setRealValue(newValue);
       } else {
         setRealValue(undefined);
-        setSelectValue(undefined);
       }
     } else {
       if (value) {
@@ -40,102 +36,21 @@ const DateUI = (props: any) => {
     } else {
       newTime = e.format(format);
     }
-    setSelectValue(undefined);
+
     setRealValue(e);
     onChange(newTime);
   };
-  const options = [
-    { value: 1, label: '近一周' },
-    { value: 2, label: '上周' },
-    { value: 3, label: '本周' },
-    { value: 4, label: '近一月' },
-    { value: 5, label: '上月' },
-    { value: 6, label: '本月' },
-    { value: 7, label: '近一季度' },
-    { value: 8, label: '上一季度' },
-    { value: 9, label: '本季度' },
-    { value: 10, label: '近一年度' },
-    { value: 11, label: '上一年度' },
-    { value: 12, label: '本年度' },
-  ];
 
-  const changeSelect = (value: number) => {
-    let startDate, endDate;
-
-    switch (value) {
-      case 1: // 近一周
-        startDate = dayjs().subtract(1, 'week').startOf('day');
-        endDate = dayjs().endOf('day');
-        break;
-      case 2: // 上周
-        startDate = dayjs().subtract(2, 'week').startOf('week');
-        endDate = dayjs().subtract(2, 'week').endOf('week');
-        break;
-      case 3: // 本周
-        startDate = dayjs().startOf('week');
-        endDate = dayjs().endOf('week');
-        break;
-      case 4: // 近一月
-        startDate = dayjs().subtract(1, 'month').startOf('day');
-        endDate = dayjs().endOf('day');
-        break;
-      case 5: // 上月
-        startDate = dayjs().subtract(2, 'month').startOf('month');
-        endDate = dayjs().subtract(2, 'month').endOf('month');
-        break;
-      case 6: // 本月
-        startDate = dayjs().startOf('month');
-        endDate = dayjs().endOf('month');
-        break;
-      case 7: // 近一季度
-        startDate = dayjs().subtract(3, 'month').startOf('day');
-        endDate = dayjs().endOf('day');
-        break;
-      case 8: // 上季度
-        startDate = dayjs().subtract(1, 'quarter').startOf('quarter');
-        endDate = dayjs().subtract(1, 'quarter').endOf('quarter');
-        break;
-      case 9: // 本季度
-        startDate = dayjs().startOf('quarter');
-        endDate = dayjs().endOf('quarter');
-        break;
-      case 10: // 近一年
-        startDate = dayjs().subtract(1, 'year').startOf('day');
-        endDate = dayjs().endOf('day');
-        break;
-      case 11: // 上年度
-        startDate = dayjs().subtract(1, 'year').startOf('year');
-        endDate = dayjs().subtract(1, 'year').endOf('year');
-        break;
-      case 12: // 本年度
-        startDate = dayjs().startOf('year');
-        endDate = dayjs().endOf('year');
-        break;
-      default:
-        break;
-    }
-    setSelectValue(value);
-    setRealValue([startDate, endDate]);
-    if (startDate && endDate) {
-      onChange([startDate.format(format), endDate.format(format)]);
-    }
-  };
   return (
     <ConfigProvider locale={zhCN}>
       {rangePicker ? (
-        <div className={`sic-dateui ${className ?? ''}`}>
-          <DatePicker.RangePicker separator={'至'} variant="borderless" value={realValue} onChange={changeDate} {...otherProps} />
-          {isShowSelect && (
-            <Select
-              className="sic-dateui-select"
-              variant="filled"
-              value={selectValue}
-              placeholder="快捷选择"
-              options={options}
-              onChange={changeSelect}
-            />
-          )}
-        </div>
+        <DatePicker.RangePicker
+          className={`sic-dateui ${className ?? ''}`}
+          separator={'至'}
+          value={realValue}
+          onChange={changeDate}
+          {...otherProps}
+        />
       ) : (
         <DatePicker className={`sic-dateui ${className ?? ''}`} value={realValue} onChange={changeDate} {...otherProps} />
       )}
