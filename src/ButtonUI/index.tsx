a2fea09fca2d0c263ed3a2d12293cbe554b92ab2
xiaoli0705 <xiaoli980705@gmail.com>
2024-05-06 11:19:52 +0800

    feat: 更新

--- src/ButtonUI/index.tsx
+++ src/ButtonUI/index.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { Button, ButtonProps } from 'antd';
 import './index.scss';
 interface ButtonUIProps extends Omit<ButtonProps, 'type'> {
+  className?: string;
   type?: any;
   children?: React.ReactNode;
   iconPosition?: 'start' | 'end';
 }
 
 const ButtonUI = (props: ButtonUIProps) => {
-  const { type, children, iconPosition = 'end', ...otherProps } = props;
+  const { className, type, children, iconPosition = 'end', ...otherProps } = props;
   return (
-    <Button className="sic-buttonui" type={type} iconPosition={iconPosition} {...otherProps}>
+    <Button className={`sic-buttonui ${className ?? ''}`} type={type} iconPosition={iconPosition} {...otherProps}>
       {children}
     </Button>
   );
