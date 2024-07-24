1e0530c0aa6b86f8f2427cdff724580c8d4a480a
xiaoli0705 <xiaoli980705@gmail.com>
2024-07-24 11:25:04 +0800

    feat: 更新

---
+++ docs-dist/meta__ButtonUI.896ea35c.async.js
@@ -0,0 +1,64 @@
+"use strict";(self.webpackChunksic_ui=self.webpackChunksic_ui||[]).push([[2520],{92337:function(f,I,e){e.r(I),e.d(I,{demos:function(){return B}});var y=e(43953),l=e.n(y),U=e(91184),c=e.n(U),i=e(38497),E=e(86064),B={"buttonui-demo-0":{component:i.memo(i.lazy(c()(l()().mark(function p(){var o,n,a,u,s,t,d;return l()().wrap(function(r){for(;;)switch(r.prev=r.next){case 0:return r.next=2,Promise.resolve().then(e.t.bind(e,38497,19));case 2:return o=r.sent,n=o.default,r.next=6,Promise.resolve().then(e.bind(e,77635));case 6:return a=r.sent,u=a.Space,r.next=10,Promise.resolve().then(e.bind(e,44956));case 10:return s=r.sent,t=s.ButtonUI,d=function(){return n.createElement(u,null,n.createElement(t,null,"\u9ED8\u8BA4\u6309\u94AE"),n.createElement(t,{type:"primary"},"\u4E3B\u8981\u6309\u94AE"),n.createElement(t,{type:"border"},"\u6B21\u8981\u6309\u94AE"),n.createElement(t,{type:"dashed"},"\u865A\u7EBF\u6309\u94AE"),n.createElement(t,{type:"text"},"\u6587\u672C\u6309\u94AE"),n.createElement(t,{type:"link"},"\u94FE\u63A5\u6309\u94AE"))},r.abrupt("return",{default:d});case 14:case"end":return r.stop()}},p)})))),asset:{type:"BLOCK",id:"buttonui-demo-0",refAtomIds:["ButtonUI"],dependencies:{"index.tsx":{type:"FILE",value:`import React from 'react';
+import { Space } from 'antd';
+import { ButtonUI } from 'sic-ui';
+
+const App: React.FC = () => (
+  <Space>
+    <ButtonUI>\u9ED8\u8BA4\u6309\u94AE</ButtonUI>
+    <ButtonUI type="primary">\u4E3B\u8981\u6309\u94AE</ButtonUI>
+    <ButtonUI type="border">\u6B21\u8981\u6309\u94AE</ButtonUI>
+    <ButtonUI type="dashed">\u865A\u7EBF\u6309\u94AE</ButtonUI>
+    <ButtonUI type="text">\u6587\u672C\u6309\u94AE</ButtonUI>
+    <ButtonUI type="link">\u94FE\u63A5\u6309\u94AE</ButtonUI>
+  </Space>
+);
+export default App;`},react:{type:"NPM",value:"18.3.1"},antd:{type:"NPM",value:"5.19.3"},"sic-ui":{type:"NPM",value:"0.3.54"}},entry:"index.tsx"},context:{react:e(38497),antd:e(77635),"sic-ui":e(44956)},renderOpts:{compile:function(){var p=c()(l()().mark(function n(){var a,u=arguments;return l()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,e.e(6628).then(e.bind(e,76628));case 2:return t.abrupt("return",(a=t.sent).default.apply(a,u));case 3:case"end":return t.stop()}},n)}));function o(){return p.apply(this,arguments)}return o}()}},"buttonui-demo-1":{component:i.memo(i.lazy(c()(l()().mark(function p(){var o,n,a,u,s,t,d;return l()().wrap(function(r){for(;;)switch(r.prev=r.next){case 0:return r.next=2,Promise.resolve().then(e.t.bind(e,38497,19));case 2:return o=r.sent,n=o.default,r.next=6,Promise.resolve().then(e.bind(e,77635));case 6:return a=r.sent,u=a.Space,r.next=10,Promise.resolve().then(e.bind(e,44956));case 10:return s=r.sent,t=s.ButtonUI,d=function(){return n.createElement(u,null,n.createElement(t,{danger:!0},"\u9ED8\u8BA4\u6309\u94AE"),n.createElement(t,{type:"primary",danger:!0},"\u4E3B\u8981\u6309\u94AE"),n.createElement(t,{type:"border",danger:!0},"\u6B21\u8981\u6309\u94AE"),n.createElement(t,{type:"dashed",danger:!0},"\u865A\u7EBF\u6309\u94AE"),n.createElement(t,{type:"text",danger:!0},"\u6587\u672C\u6309\u94AE"),n.createElement(t,{type:"link",danger:!0},"\u94FE\u63A5\u6309\u94AE"))},r.abrupt("return",{default:d});case 14:case"end":return r.stop()}},p)})))),asset:{type:"BLOCK",id:"buttonui-demo-1",refAtomIds:["ButtonUI"],dependencies:{"index.tsx":{type:"FILE",value:`import React from 'react';
+import { Space } from 'antd';
+import { ButtonUI } from 'sic-ui';
+
+const App: React.FC = () => (
+  <Space>
+    <ButtonUI danger>\u9ED8\u8BA4\u6309\u94AE</ButtonUI>
+    <ButtonUI type="primary" danger>
+      \u4E3B\u8981\u6309\u94AE
+    </ButtonUI>
+    <ButtonUI type="border" danger>
+      \u6B21\u8981\u6309\u94AE
+    </ButtonUI>
+    <ButtonUI type="dashed" danger>
+      \u865A\u7EBF\u6309\u94AE
+    </ButtonUI>
+    <ButtonUI type="text" danger>
+      \u6587\u672C\u6309\u94AE
+    </ButtonUI>
+    <ButtonUI type="link" danger>
+      \u94FE\u63A5\u6309\u94AE
+    </ButtonUI>
+  </Space>
+);
+export default App;`},react:{type:"NPM",value:"18.3.1"},antd:{type:"NPM",value:"5.19.3"},"sic-ui":{type:"NPM",value:"0.3.54"}},entry:"index.tsx"},context:{react:e(38497),antd:e(77635),"sic-ui":e(44956)},renderOpts:{compile:function(){var p=c()(l()().mark(function n(){var a,u=arguments;return l()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,e.e(6628).then(e.bind(e,76628));case 2:return t.abrupt("return",(a=t.sent).default.apply(a,u));case 3:case"end":return t.stop()}},n)}));function o(){return p.apply(this,arguments)}return o}()}},"buttonui-demo-2":{component:i.memo(i.lazy(c()(l()().mark(function p(){var o,n,a,u,s,t,d,v;return l()().wrap(function(m){for(;;)switch(m.prev=m.next){case 0:return m.next=2,Promise.resolve().then(e.t.bind(e,38497,19));case 2:return o=m.sent,n=o.default,m.next=6,Promise.resolve().then(e.bind(e,77635));case 6:return a=m.sent,u=a.Form,s=a.Space,m.next=11,Promise.resolve().then(e.bind(e,44956));case 11:return t=m.sent,d=t.ButtonUI,v=function(){return n.createElement(u,{disabled:!0},n.createElement(s,null,n.createElement(d,{danger:!0},"\u9ED8\u8BA4\u6309\u94AE"),n.createElement(d,{type:"primary"},"\u4E3B\u8981\u6309\u94AE"),n.createElement(d,{type:"border",danger:!0},"\u5220\u9664\u6309\u94AE")))},m.abrupt("return",{default:v});case 15:case"end":return m.stop()}},p)})))),asset:{type:"BLOCK",id:"buttonui-demo-2",refAtomIds:["ButtonUI"],dependencies:{"index.tsx":{type:"FILE",value:`import React from 'react';
+import { Form, Space } from 'antd';
+import { ButtonUI } from 'sic-ui';
+
+const App: React.FC = () => (
+  <Form disabled={true}>
+    <Space>
+      <ButtonUI danger>\u9ED8\u8BA4\u6309\u94AE</ButtonUI>
+      <ButtonUI type="primary">\u4E3B\u8981\u6309\u94AE</ButtonUI>
+      <ButtonUI type="border" danger>
+        \u5220\u9664\u6309\u94AE
+      </ButtonUI>
+    </Space>
+  </Form>
+);
+export default App;`},react:{type:"NPM",value:"18.3.1"},antd:{type:"NPM",value:"5.19.3"},"sic-ui":{type:"NPM",value:"0.3.54"}},entry:"index.tsx"},context:{react:e(38497),antd:e(77635),"sic-ui":e(44956)},renderOpts:{compile:function(){var p=c()(l()().mark(function n(){var a,u=arguments;return l()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,e.e(6628).then(e.bind(e,76628));case 2:return t.abrupt("return",(a=t.sent).default.apply(a,u));case 3:case"end":return t.stop()}},n)}));function o(){return p.apply(this,arguments)}return o}()}},"buttonui-demo-3":{component:i.memo(i.lazy(c()(l()().mark(function p(){var o,n,a,u,s;return l()().wrap(function(d){for(;;)switch(d.prev=d.next){case 0:return d.next=2,Promise.resolve().then(e.bind(e,77635));case 2:return o=d.sent,n=o.Space,d.next=6,Promise.resolve().then(e.bind(e,44956));case 6:return a=d.sent,u=a.ButtonUI,s=a.IconUI,d.abrupt("return",{default:function(){return i.createElement(n,null,i.createElement(u,{icon:i.createElement(s,{name:"Export"})},"\u6309\u94AE"),i.createElement(u,{icon:i.createElement(s,{name:"Export"}),iconPosition:"start"},"\u6309\u94AE"))}});case 10:case"end":return d.stop()}},p)})))),asset:{type:"BLOCK",id:"buttonui-demo-3",refAtomIds:["ButtonUI"],dependencies:{"index.tsx":{type:"FILE",value:`import { Space } from 'antd';
+import { ButtonUI, IconUI } from 'sic-ui';
+
+export default () => (
+  <Space>
+    <ButtonUI icon={<IconUI name="Export" />}>\u6309\u94AE</ButtonUI>
+    <ButtonUI icon={<IconUI name="Export" />} iconPosition="start">
+      \u6309\u94AE
+    </ButtonUI>
+  </Space>
+);`},antd:{type:"NPM",value:"5.19.3"},"sic-ui":{type:"NPM",value:"0.3.54"}},entry:"index.tsx"},context:{antd:e(77635),"sic-ui":e(44956)},renderOpts:{compile:function(){var p=c()(l()().mark(function n(){var a,u=arguments;return l()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,e.e(6628).then(e.bind(e,76628));case 2:return t.abrupt("return",(a=t.sent).default.apply(a,u));case 3:case"end":return t.stop()}},n)}));function o(){return p.apply(this,arguments)}return o}()}}}},56106:function(f,I,e){e.r(I),e.d(I,{texts:function(){return l}});var y=e(86064);const l=[{value:"\u56FD\u4FE1\u7075\u5DE5\u540E\u53F0\u7BA1\u7406\u7CFB\u7EDF-\u6309\u94AE\u7EC4\u4EF6",paraId:0},{value:"\u{1F48E} \u57FA\u7840\u4F7F\u7528",paraId:1,tocIndex:2},{value:"\u{1F48E} \u5371\u9669\u6309\u94AE",paraId:2},{value:"\u{1F48E} \u8868\u5355\u4E2D\u6309\u94AE\u7F6E\u7070",paraId:3},{value:"\u{1F48E} \u4F7F\u7528 Icon",paraId:4},{value:"\u{1F525}TableUI \u5F15\u7528 Ant Design \u7EC4\u4EF6\u5E93\uFF0C\u5728\u4F7F\u7528\u8FC7\u7A0B\u4E2D\u4E0D\u505A\u5982\u4F55\u4FB5\u5165\u6027\u7684\u66F4\u6539",paraId:5,tocIndex:3},{value:"\u5C5E\u6027",paraId:6,tocIndex:3},{value:"\u8BF4\u660E",paraId:6,tocIndex:3},{value:"\u7C7B\u578B",paraId:6,tocIndex:3},{value:"\u9ED8\u8BA4\u503C",paraId:6,tocIndex:3},{value:"\u662F\u5426\u5FC5\u4F20",paraId:6,tocIndex:3},{value:"type",paraId:6,tocIndex:3},{value:"\u7C7B\u578B",paraId:6,tocIndex:3},{value:"String",paraId:6,tocIndex:3},{value:"default",paraId:6,tocIndex:3},{value:"\u5426",paraId:6,tocIndex:3},{value:"icon",paraId:6,tocIndex:3},{value:"icon",paraId:6,tocIndex:3},{value:"React.ReactNode",paraId:6,tocIndex:3},{value:"-",paraId:6,tocIndex:3},{value:"\u5426",paraId:6,tocIndex:3},{value:"className",paraId:6,tocIndex:3},{value:"\u7C7B\u540D",paraId:6,tocIndex:3},{value:"String",paraId:6,tocIndex:3},{value:"-",paraId:6,tocIndex:3},{value:"\u5426",paraId:6,tocIndex:3},{value:"style",paraId:6,tocIndex:3},{value:"\u6837\u5F0F",paraId:6,tocIndex:3},{value:"React.CSSProperties",paraId:6,tocIndex:3},{value:"-",paraId:6,tocIndex:3},{value:"\u5426",paraId:6,tocIndex:3},{value:"children",paraId:6,tocIndex:3},{value:"\u5185\u5BB9",paraId:6,tocIndex:3},{value:"React.ReactNode",paraId:6,tocIndex:3},{value:"-",paraId:6,tocIndex:3},{value:"\u5426",paraId:6,tocIndex:3}]}}]);
---
+++ docs-dist/meta__NotificationUI.b5e645eb.async.js
@@ -0,0 +1,47 @@
+"use strict";(self.webpackChunksic_ui=self.webpackChunksic_ui||[]).push([[2408],{45417:function(x,r,e){e.r(r),e.d(r,{demos:function(){return P}});var m=e(43953),o=e.n(m),U=e(9083),y=e.n(U),g=e(91184),_=e.n(g),v=e(38497),O=e(60997),P={"notificationui-demo-0":{component:v.memo(v.lazy(_()(o()().mark(function c(){var d,n,s,u,I,t,f,i,E;return o()().wrap(function(a){for(;;)switch(a.prev=a.next){case 0:return a.next=2,Promise.resolve().then(e.t.bind(e,38497,19));case 2:return d=a.sent,n=d.default,s=d.useState,a.next=7,Promise.resolve().then(e.bind(e,77635));case 7:return u=a.sent,I=u.Space,a.next=11,Promise.resolve().then(e.bind(e,44956));case 11:return t=a.sent,f=t.NotificationUI,i=t.ButtonUI,E=function(){var B=s(),C=y()(B,2),M=C[0],l=C[1];return n.createElement("div",null,n.createElement(I,null,n.createElement(i,{onClick:function(){l({type:"success",description:"\u6210\u529F\u63D0\u793A\u6D88\u606F"})}},"\u6210\u529F"),n.createElement(i,{onClick:function(){l({type:"error",description:"\u5931\u8D25\u63D0\u793A\u6D88\u606F"})}},"\u5931\u8D25"),n.createElement(i,{onClick:function(){l({type:"info",description:"\u63D0\u793A\u6D88\u606F"})}},"\u63D0\u793A"),n.createElement(i,{onClick:function(){l({type:"warning",description:"\u8B66\u544A\u63D0\u793A\u6D88\u606F"})}},"\u8B66\u544A")),n.createElement(f,{notmsg:M}))},a.abrupt("return",{default:E});case 16:case"end":return a.stop()}},c)})))),asset:{type:"BLOCK",id:"notificationui-demo-0",refAtomIds:["NotificationUI"],dependencies:{"index.tsx":{type:"FILE",value:`import React, { useState } from 'react';
+import { Space } from 'antd';
+import { NotificationUI, ButtonUI } from 'sic-ui';
+
+const App: React.FC = () => {
+  // \u63D0\u793A\u6D88\u606F
+  const [notmsg, setNotmsg] = useState();
+
+  return (
+    <div>
+      <Space>
+        <ButtonUI
+          onClick={() => {
+            setNotmsg({ type: 'success', description: '\u6210\u529F\u63D0\u793A\u6D88\u606F' });
+          }}
+        >
+          \u6210\u529F
+        </ButtonUI>
+        <ButtonUI
+          onClick={() => {
+            setNotmsg({ type: 'error', description: '\u5931\u8D25\u63D0\u793A\u6D88\u606F' });
+          }}
+        >
+          \u5931\u8D25
+        </ButtonUI>
+        <ButtonUI
+          onClick={() => {
+            setNotmsg({ type: 'info', description: '\u63D0\u793A\u6D88\u606F' });
+          }}
+        >
+          \u63D0\u793A
+        </ButtonUI>
+        <ButtonUI
+          onClick={() => {
+            setNotmsg({ type: 'warning', description: '\u8B66\u544A\u63D0\u793A\u6D88\u606F' });
+          }}
+        >
+          \u8B66\u544A
+        </ButtonUI>
+      </Space>
+      <NotificationUI notmsg={notmsg} />
+    </div>
+  );
+};
+export default App;`},react:{type:"NPM",value:"18.3.1"},antd:{type:"NPM",value:"5.19.3"},"sic-ui":{type:"NPM",value:"0.3.54"}},entry:"index.tsx"},context:{react:e(38497),antd:e(77635),"sic-ui":e(44956)},renderOpts:{compile:function(){var c=_()(o()().mark(function n(){var s,u=arguments;return o()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,e.e(6628).then(e.bind(e,76628));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,u));case 3:case"end":return t.stop()}},n)}));function d(){return c.apply(this,arguments)}return d}()}}}},37465:function(x,r,e){e.r(r),e.d(r,{texts:function(){return o}});var m=e(60997);const o=[{value:`\u56FD\u4FE1\u7075\u5DE5\u540E\u53F0\u7BA1\u7406\u7CFB\u7EDF-\u6D88\u606F\u63D0\u9192\u7EC4\u4EF6
+`,paraId:0},{value:`
+\u539F\u56E0\uFF1A\u53F3\u4E0A\u89D2\u770B\u4E0D\u6E05`,paraId:0},{value:"\u{1F48E} \u6D88\u606F\u63D0\u9192\u7EC4\u4EF6",paraId:1,tocIndex:2},{value:"\u{1F525}TableUI \u5F15\u7528 Ant Design \u7EC4\u4EF6\u5E93\uFF0C\u5728\u4F7F\u7528\u8FC7\u7A0B\u4E2D\u4E0D\u505A\u5982\u4F55\u4FB5\u5165\u6027\u7684\u66F4\u6539",paraId:2,tocIndex:3},{value:"\u5C5E\u6027",paraId:3,tocIndex:3},{value:"\u8BF4\u660E",paraId:3,tocIndex:3},{value:"\u7C7B\u578B",paraId:3,tocIndex:3},{value:"\u9ED8\u8BA4\u503C",paraId:3,tocIndex:3},{value:"\u662F\u5426\u5FC5\u4F20",paraId:3,tocIndex:3},{value:"notimsg",paraId:3,tocIndex:3},{value:"\u63D0\u793A\u6D88\u606F",paraId:3,tocIndex:3},{value:"Object",paraId:3,tocIndex:3},{value:"-",paraId:3,tocIndex:3},{value:"\u662F",paraId:3,tocIndex:3},{value:"\u5C5E\u6027",paraId:4,tocIndex:4},{value:"\u8BF4\u660E",paraId:4,tocIndex:4},{value:"\u7C7B\u578B",paraId:4,tocIndex:4},{value:"\u9ED8\u8BA4\u503C",paraId:4,tocIndex:4},{value:"\u662F\u5426\u5FC5\u4F20",paraId:4,tocIndex:4},{value:"type",paraId:4,tocIndex:4},{value:"\u7C7B\u578B",paraId:4,tocIndex:4},{value:"success error info warning",paraId:4,tocIndex:4},{value:"-",paraId:4,tocIndex:4},{value:"\u662F",paraId:4,tocIndex:4},{value:"description",paraId:4,tocIndex:4},{value:"\u63D0\u793A\u6D88\u606F",paraId:4,tocIndex:4},{value:"String",paraId:4,tocIndex:4},{value:"-",paraId:4,tocIndex:4},{value:"\u662F",paraId:4,tocIndex:4},{value:"message",paraId:4,tocIndex:4},{value:"\u63D0\u793A\u6D88\u606F",paraId:4,tocIndex:4},{value:"String",paraId:4,tocIndex:4},{value:"\u7CFB\u7EDF\u63D0\u793A",paraId:4,tocIndex:4},{value:"\u5426",paraId:4,tocIndex:4}]}}]);
