0f3233fda8005ff7b9853c087b2ffe38a552a175
xiaoli0705 <xiaoli980705@gmail.com>
2024-07-26 13:05:05 +0800

    feat: 更新

---
+++ docs-dist/meta__CellUI.c53b1bcc.async.js
@@ -0,0 +1,33 @@
+"use strict";(self.webpackChunksic_ui=self.webpackChunksic_ui||[]).push([[2647],{38411:function(v,i,e){e.r(i),e.d(i,{demos:function(){return C}});var I=e(43953),r=e.n(I),f=e(91184),p=e.n(f),m=e(38497),E=e(49917),C={"cellui-demo-0":{component:m.memo(m.lazy(p()(r()().mark(function s(){var d,a,l,o,u,n,c;return r()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,Promise.resolve().then(e.t.bind(e,38497,19));case 2:return d=t.sent,a=d.default,t.next=6,Promise.resolve().then(e.bind(e,77635));case 6:return l=t.sent,o=l.Space,t.next=10,Promise.resolve().then(e.bind(e,49154));case 10:return u=t.sent,n=u.CellUI,c=function(){return a.createElement(o,{wrap:!0},a.createElement("div",{style:{width:100,backgroundColor:"#dff8e7"}},a.createElement(n,{line:1},"\u660E\u6708\u677E\u95F4\u7167")),a.createElement("div",{style:{width:100,backgroundColor:"#dff8e7"}},a.createElement(n,{line:1},"\u660E\u6708\u677E\u95F4\u7167\uFF0C\u6E05\u6CC9\u77F3\u4E0A\u6D41\u3002")))},t.abrupt("return",{default:c});case 14:case"end":return t.stop()}},s)})))),asset:{type:"BLOCK",id:"cellui-demo-0",refAtomIds:["CellUI"],dependencies:{"index.tsx":{type:"FILE",value:`import React from 'react';
+import { Space } from 'antd';
+import { CellUI } from 'sic-ui';
+
+const App: React.FC = () => {
+  return (
+    <Space wrap>
+      <div style={{ width: 100, backgroundColor: '#dff8e7' }}>
+        <CellUI line={1}>\u660E\u6708\u677E\u95F4\u7167</CellUI>
+      </div>
+      <div style={{ width: 100, backgroundColor: '#dff8e7' }}>
+        <CellUI line={1}>\u660E\u6708\u677E\u95F4\u7167\uFF0C\u6E05\u6CC9\u77F3\u4E0A\u6D41\u3002</CellUI>
+      </div>
+    </Space>
+  );
+};
+export default App;`},react:{type:"NPM",value:"18.3.1"},antd:{type:"NPM",value:"5.19.3"},"sic-ui":{type:"NPM",value:"0.3.56"}},entry:"index.tsx"},context:{react:e(38497),antd:e(77635),"sic-ui":e(49154)},renderOpts:{compile:function(){var s=p()(r()().mark(function a(){var l,o=arguments;return r()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(6628).then(e.bind(e,76628));case 2:return n.abrupt("return",(l=n.sent).default.apply(l,o));case 3:case"end":return n.stop()}},a)}));function d(){return s.apply(this,arguments)}return d}()}},"cellui-demo-1":{component:m.memo(m.lazy(p()(r()().mark(function s(){var d,a,l,o,u,n,c;return r()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,Promise.resolve().then(e.t.bind(e,38497,19));case 2:return d=t.sent,a=d.default,t.next=6,Promise.resolve().then(e.bind(e,77635));case 6:return l=t.sent,o=l.Space,t.next=10,Promise.resolve().then(e.bind(e,49154));case 10:return u=t.sent,n=u.CellUI,c=function(){return a.createElement(o,{wrap:!0},a.createElement("div",{style:{width:100,backgroundColor:"#dff8e7"}},a.createElement(n,{line:2},"\u5357\u6751\u7FA4\u7AE5\u6B3A\u6211\u8001\u65E0\u529B")),a.createElement("div",{style:{width:100,backgroundColor:"#dff8e7"}},a.createElement(n,{line:2},"\u5E8A\u524D\u660E\u6708\u5149\uFF0C\u7591\u662F\u5730\u4E0A\u971C\u3002\u4E3E\u5934\u671B\u660E\u6708\uFF0C\u4F4E\u5934\u601D\u6545\u4E61\u3002")))},t.abrupt("return",{default:c});case 14:case"end":return t.stop()}},s)})))),asset:{type:"BLOCK",id:"cellui-demo-1",refAtomIds:["CellUI"],dependencies:{"index.tsx":{type:"FILE",value:`import React from 'react';
+import { Space } from 'antd';
+import { CellUI } from 'sic-ui';
+
+const App: React.FC = () => {
+  return (
+    <Space wrap>
+      <div style={{ width: 100, backgroundColor: '#dff8e7' }}>
+        <CellUI line={2}>\u5357\u6751\u7FA4\u7AE5\u6B3A\u6211\u8001\u65E0\u529B</CellUI>
+      </div>
+      <div style={{ width: 100, backgroundColor: '#dff8e7' }}>
+        <CellUI line={2}>\u5E8A\u524D\u660E\u6708\u5149\uFF0C\u7591\u662F\u5730\u4E0A\u971C\u3002\u4E3E\u5934\u671B\u660E\u6708\uFF0C\u4F4E\u5934\u601D\u6545\u4E61\u3002</CellUI>
+      </div>
+    </Space>
+  );
+};
+export default App;`},react:{type:"NPM",value:"18.3.1"},antd:{type:"NPM",value:"5.19.3"},"sic-ui":{type:"NPM",value:"0.3.56"}},entry:"index.tsx"},context:{react:e(38497),antd:e(77635),"sic-ui":e(49154)},renderOpts:{compile:function(){var s=p()(r()().mark(function a(){var l,o=arguments;return r()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(6628).then(e.bind(e,76628));case 2:return n.abrupt("return",(l=n.sent).default.apply(l,o));case 3:case"end":return n.stop()}},a)}));function d(){return s.apply(this,arguments)}return d}()}}}},58885:function(v,i,e){e.r(i),e.d(i,{texts:function(){return r}});var I=e(49917);const r=[{value:"\u56FD\u4FE1\u7075\u5DE5\u540E\u53F0\u7BA1\u7406\u7CFB\u7EDF-\u5355\u5143\u683C\u7EC4\u4EF6",paraId:0},{value:"\u{1F48E} \u5355\u884C\u6548\u679C",paraId:1,tocIndex:1},{value:"\u{1F48E} \u591A\u884C\u6548\u679C",paraId:2},{value:"\u5C5E\u6027",paraId:3,tocIndex:2},{value:"\u8BF4\u660E",paraId:3,tocIndex:2},{value:"\u7C7B\u578B",paraId:3,tocIndex:2},{value:"\u9ED8\u8BA4\u503C",paraId:3,tocIndex:2},{value:"\u662F\u5426\u5FC5\u4F20",paraId:3,tocIndex:2},{value:"line",paraId:3,tocIndex:2},{value:"\u884C\u6570\u51FA\u73B0\u6EA2\u51FA\u6548\u679C",paraId:3,tocIndex:2},{value:"Number",paraId:3,tocIndex:2},{value:"2",paraId:3,tocIndex:2},{value:"\u5426",paraId:3,tocIndex:2},{value:"isShowCopy",paraId:3,tocIndex:2},{value:"\u6EA2\u51FA\u5185\u5BB9\u662F\u5426\u663E\u793A\u590D\u5236",paraId:3,tocIndex:2},{value:"Boolean",paraId:3,tocIndex:2},{value:"false",paraId:3,tocIndex:2},{value:"\u5426",paraId:3,tocIndex:2},{value:"children",paraId:3,tocIndex:2},{value:"\u5185\u5BB9",paraId:3,tocIndex:2},{value:"ReactNode",paraId:3,tocIndex:2},{value:"-",paraId:3,tocIndex:2},{value:"\u5426",paraId:3,tocIndex:2}]}}]);
