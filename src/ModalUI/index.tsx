4409ce27edef1cc59bfbd6267c98a7ae122133ac
lmy980705 <xiaoli980705@gmail.com>
2024-08-09 15:10:40 +0800

    feat: modal支持拖动

--- src/ModalUI/index.tsx
+++ src/ModalUI/index.tsx
@@ -1,5 +1,7 @@
-import React, { Fragment } from 'react';
+import React, { useRef, useState, Fragment } from 'react';
 import { Modal } from 'antd';
+import type { DraggableData, DraggableEvent } from 'react-draggable';
+import Draggable from 'react-draggable';
 import './index.scss';
 
 const RenderTitle = (title: string, icon: React.ReactNode, type?: string) => {
@@ -17,14 +19,49 @@ const RenderTitle = (title: string, icon: React.ReactNode, type?: string) => {
 
 const ModalUI = (props: any) => {
   const { className, isOpen, setIsOpen, title, icon, type, confirm, children, ...otherProps } = props;
+  const [disabled, setDisabled] = useState(true);
+  const [bounds, setBounds] = useState({ left: 0, top: 0, bottom: 0, right: 0 });
+  const draggleRef = useRef<HTMLDivElement>(null);
+
+  const onStart = (_event: DraggableEvent, uiData: DraggableData) => {
+    const { clientWidth, clientHeight } = window.document.documentElement;
+    const targetRect = draggleRef.current?.getBoundingClientRect();
+    if (!targetRect) {
+      return;
+    }
+    setBounds({
+      left: -targetRect.left + uiData.x,
+      right: clientWidth - (targetRect.right - uiData.x),
+      top: -targetRect.top + uiData.y,
+      bottom: clientHeight - (targetRect.bottom - uiData.y),
+    });
+  };
+
   return (
     <Modal
       className={`sicModalui ${className ?? ''} ${type ?? ''}`}
       open={isOpen}
-      title={RenderTitle(title, icon, type)}
-      closeIcon={!icon}
+      title={
+        <div
+          style={{ width: '100%', cursor: 'move' }}
+          onMouseOver={() => {
+            if (disabled) {
+              setDisabled(false);
+            }
+          }}
+          onMouseOut={() => setDisabled(true)}
+        >
+          {RenderTitle(title, icon, type)}
+        </div>
+      }
       onCancel={() => setIsOpen(undefined)}
       onOk={confirm}
+      closeIcon={!icon}
+      modalRender={(modal) => (
+        <Draggable disabled={disabled} bounds={bounds} nodeRef={draggleRef} onStart={(event, uiData) => onStart(event, uiData)}>
+          <div ref={draggleRef}>{modal}</div>
+        </Draggable>
+      )}
       {...otherProps}
     >
       {children}
