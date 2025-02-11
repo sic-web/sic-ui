21dfa59249bf67110282ff239bfa5326943a74cf
lmy1818 <lmy1818@users.noreply.github.com>
2025-02-11 07:34:03 +0000

    deploy: 4564efea313b00c64fc9c71acc1d1f988748a84d

--- src/IconUI/index.tsx
+++
@@ -1,8 +0,0 @@
-import { iconRally } from './material/rally';
-const IconUI = (props: { name: string; [key: string]: any }) => {
-  const { name, ...otherProps } = props;
-  const current: any = iconRally?.filter((item) => item?.name === name)?.[0];
-  return current?.element({ ...otherProps });
-};
-
-export default IconUI;
