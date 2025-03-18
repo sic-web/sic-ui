f5cb3c5e534f6a8d42739ad09c541f764614e6bd
lmy1818 <lmy1818@users.noreply.github.com>
2025-03-18 09:58:12 +0000

    deploy: 3ec61509a8dea6815774696202fbecb9141cb4ae

---
+++ meta__TextUI.e24c72d1.async.js
@@ -0,0 +1,15 @@
+"use strict";(self.webpackChunksic_ui=self.webpackChunksic_ui||[]).push([[3632],{95324:function(p,s,e){e.r(s),e.d(s,{demos:function(){return x}});var m=e(15009),a=e.n(m),I=e(99289),c=e.n(I),r=e(67294),f=e(58384),v=e(71612),E=e(42081),x={"textui-demo-0":{component:r.memo(r.lazy(c()(a()().mark(function i(){var _,l,d,u,o;return a()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,Promise.resolve().then(e.bind(e,71612));case 2:return _=n.sent,l=_.Space,n.next=6,Promise.resolve().then(e.bind(e,42081));case 6:return d=n.sent,u=d.TextUI,o=function(){return r.createElement(l,null,r.createElement(u,null,"\u6587\u672C"),r.createElement(u,{type:"default"},"\u6587\u672C"),r.createElement(u,{type:"danger"},"\u6587\u672C"))},n.abrupt("return",{default:o});case 10:case"end":return n.stop()}},i)})))),asset:{type:"BLOCK",id:"textui-demo-0",refAtomIds:["TextUI"],dependencies:{"index.tsx":{type:"FILE",value:`import { useState } from 'react';
+import { Space } from 'antd';
+import { TextUI } from 'sic-ui';
+
+const App: React.FC = () => {
+  return (
+    <Space>
+      <TextUI>\u6587\u672C</TextUI>
+      <TextUI type="default">\u6587\u672C</TextUI>
+      <TextUI type="danger">\u6587\u672C</TextUI>
+    </Space>
+  );
+};
+export default App;`},antd:{type:"NPM",value:"5.24.4"},"sic-ui":{type:"NPM",value:"0.5.2"}},entry:"index.tsx"},context:{antd:v,"sic-ui":E},renderOpts:{compile:function(){var i=c()(a()().mark(function l(){var d,u=arguments;return a()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,e.e(7335).then(e.bind(e,37335));case 2:return t.abrupt("return",(d=t.sent).default.apply(d,u));case 3:case"end":return t.stop()}},l)}));function _(){return i.apply(this,arguments)}return _}()}}}},69510:function(p,s,e){e.r(s),e.d(s,{texts:function(){return a}});var m=e(58384);const a=[{value:"\u56FD\u4FE1\u7075\u5DE5\u540E\u53F0\u7BA1\u7406\u7CFB\u7EDF-\u6587\u672C\u7EC4\u4EF6",paraId:0},{value:`\u{1F48E} \u57FA\u7840\u4F7F\u7528
+\u5185\u5BB9\u6837\u5F0F\u4F1A\u505A\u7EE7\u627F\uFF0C\u60AC\u505C\u51FA\u73B0\u5C0F\u624B\u6837\u5F0F`,paraId:1,tocIndex:2},{value:"\u{1F525}TableUI \u5F15\u7528 Ant Design \u7EC4\u4EF6\u5E93\uFF0C\u5728\u4F7F\u7528\u8FC7\u7A0B\u4E2D\u4E0D\u505A\u5982\u4F55\u4FB5\u5165\u6027\u7684\u66F4\u6539",paraId:2,tocIndex:3},{value:"\u5C5E\u6027",paraId:3,tocIndex:3},{value:"\u8BF4\u660E",paraId:3,tocIndex:3},{value:"\u7C7B\u578B",paraId:3,tocIndex:3},{value:"\u9ED8\u8BA4\u503C",paraId:3,tocIndex:3},{value:"\u662F\u5426\u5FC5\u4F20",paraId:3,tocIndex:3},{value:"type",paraId:3,tocIndex:3},{value:"\u7C7B\u578B",paraId:3,tocIndex:3},{value:"string",paraId:3,tocIndex:3},{value:"default",paraId:3,tocIndex:3},{value:"\u5426",paraId:3,tocIndex:3}]}}]);
