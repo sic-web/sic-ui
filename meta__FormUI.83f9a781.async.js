b951766b608280df00a557ca3eec1e5710b3db56
lmy1818 <lmy1818@users.noreply.github.com>
2025-07-24 06:37:37 +0000

    deploy: f4a86c113c13deae636b00fe47ab35504042e425

---
+++ meta__FormUI.83f9a781.async.js
@@ -0,0 +1,17 @@
+"use strict";(self.webpackChunksic_ui=self.webpackChunksic_ui||[]).push([[2538],{7373:function(p,l,e){var o;e.r(l),e.d(l,{demos:function(){return E}});var _=e(15009),d=e.n(_),v=e(99289),I=e.n(v),i=e(67294),f=e(5271),E={"formui-demo-0":{component:i.memo(i.lazy(I()(d()().mark(function m(){var s,r,u,a,c;return d()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,Promise.resolve().then(e.t.bind(e,67294,19));case 2:return s=n.sent,r=s.default,n.next=6,Promise.resolve().then(e.bind(e,5271));case 6:return u=n.sent,a=u.FormUI,c=function(){return r.createElement("div",null,r.createElement(a.ProTitle,null),r.createElement("br",null),r.createElement(a.ProTitle,{title:"\u8868\u5355\u6807\u9898"},"123123"),r.createElement("br",null),r.createElement(a.ProTitle,{required:!1}),r.createElement("br",null),r.createElement(a.ProTitle,{required:!1,line:!1}))},n.abrupt("return",{default:c});case 10:case"end":return n.stop()}},m)})))),asset:{type:"BLOCK",id:"formui-demo-0",refAtomIds:["FormUI"],dependencies:{"index.tsx":{type:"FILE",value:`import React, { useState } from 'react';
+import { FormUI } from 'sic-ui';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <FormUI.ProTitle />
+      <br />
+      <FormUI.ProTitle title="\u8868\u5355\u6807\u9898">123123</FormUI.ProTitle>
+      <br />
+      <FormUI.ProTitle required={false} />
+      <br />
+      <FormUI.ProTitle required={false} line={false} />
+    </div>
+  );
+};
+export default App;`},react:{type:"NPM",value:"18.3.1"},"sic-ui":{type:"NPM",value:"0.8.6"}},entry:"index.tsx"},context:{react:o||(o=e.t(i,2)),"sic-ui":f},renderOpts:{compile:function(){var m=I()(d()().mark(function r(){var u,a=arguments;return d()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,e.e(250).then(e.bind(e,90250));case 2:return t.abrupt("return",(u=t.sent).default.apply(u,a));case 3:case"end":return t.stop()}},r)}));function s(){return m.apply(this,arguments)}return s}()}}}},91637:function(p,l,e){e.r(l),e.d(l,{texts:function(){return o}});const o=[{value:"\u56FD\u4FE1\u7075\u5DE5\u540E\u53F0\u7BA1\u7406\u7CFB\u7EDF-\u52A0\u8F7D\u4E2D\u7EC4\u4EF6",paraId:0},{value:"\u{1F48E} \u8868\u5355\u7EC4\u4EF6-\u6807\u9898\u4F7F\u7528\uFF08\u65B0\uFF09",paraId:1,tocIndex:2},{value:"\u5C5E\u6027",paraId:2,tocIndex:3},{value:"\u8BF4\u660E",paraId:2,tocIndex:3},{value:"\u7C7B\u578B",paraId:2,tocIndex:3},{value:"\u9ED8\u8BA4\u503C",paraId:2,tocIndex:3},{value:"\u662F\u5426\u5FC5\u4F20",paraId:2,tocIndex:3},{value:"required",paraId:2,tocIndex:3},{value:"\u662F\u5426\u5FC5\u586B",paraId:2,tocIndex:3},{value:"boolean",paraId:2,tocIndex:3},{value:"true",paraId:2,tocIndex:3},{value:"\u5426",paraId:2,tocIndex:3},{value:"line",paraId:2,tocIndex:3},{value:"\u662F\u5426\u6709\u6A2A\u7EBF",paraId:2,tocIndex:3},{value:"boolean",paraId:2,tocIndex:3},{value:"true",paraId:2,tocIndex:3},{value:"\u5426",paraId:2,tocIndex:3},{value:"children",paraId:2,tocIndex:3},{value:"\u5185\u5BB9",paraId:2,tocIndex:3},{value:"ReactNode",paraId:2,tocIndex:3},{value:"null",paraId:2,tocIndex:3},{value:"\u5426",paraId:2,tocIndex:3}]}}]);
