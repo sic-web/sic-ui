9fb1ef596288555612d01c77619767ffdadc402d
lmy1818 <lmy1818@users.noreply.github.com>
2025-07-28 09:06:22 +0000

    deploy: 35c7a7674a7495d1653082dd19216e3b2d5cb2eb

---
+++ meta__SelectUI.878f5ea5.async.js
@@ -0,0 +1,21 @@
+"use strict";(self.webpackChunksic_ui=self.webpackChunksic_ui||[]).push([[6139],{9645:function(_,u,e){e.r(u),e.d(u,{demos:function(){return I}});var o=e(15009),t=e.n(o),m=e(99289),c=e.n(m),a=e(67294),p=e(11952),I={"selectui-demo-0":{component:a.memo(a.lazy(c()(t()().mark(function d(){var s,r;return t()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,Promise.resolve().then(e.bind(e,11952));case 2:return s=n.sent,r=s.SelectUI,n.abrupt("return",{default:function(){return a.createElement(a.Fragment,null,a.createElement(r,{placeholder:"\u8BF7\u9009\u62E9",options:[{value:1,label:"\u5F00\u542F"},{value:2,label:"\u5173\u95ED"}]}),a.createElement(r,{placeholder:"\u8BF7\u9009\u62E9",copy:!0,options:[{value:1,label:"\u5F00\u542F"},{value:2,label:"\u5173\u95ED"}]}))}});case 5:case"end":return n.stop()}},d)})))),asset:{type:"BLOCK",id:"selectui-demo-0",refAtomIds:["SelectUI"],dependencies:{"index.tsx":{type:"FILE",value:`import { SelectUI } from 'sic-ui';
+
+export default () => (
+  <>
+    <SelectUI
+      placeholder="\u8BF7\u9009\u62E9"
+      options={[
+        { value: 1, label: '\u5F00\u542F' },
+        { value: 2, label: '\u5173\u95ED' },
+      ]}
+    />
+    <SelectUI
+      placeholder="\u8BF7\u9009\u62E9"
+      copy={true}
+      options={[
+        { value: 1, label: '\u5F00\u542F' },
+        { value: 2, label: '\u5173\u95ED' },
+      ]}
+    />
+  </>
+);`},"sic-ui":{type:"NPM",value:"0.8.9"}},entry:"index.tsx"},context:{"sic-ui":p},renderOpts:{compile:function(){var d=c()(t()().mark(function r(){var i,n=arguments;return t()().wrap(function(l){for(;;)switch(l.prev=l.next){case 0:return l.next=2,e.e(250).then(e.bind(e,90250));case 2:return l.abrupt("return",(i=l.sent).default.apply(i,n));case 3:case"end":return l.stop()}},r)}));function s(){return d.apply(this,arguments)}return s}()}}}},8941:function(_,u,e){e.r(u),e.d(u,{texts:function(){return o}});const o=[{value:"\u56FD\u4FE1\u7075\u5DE5\u540E\u53F0\u7BA1\u7406\u7CFB\u7EDF-\u4E0B\u62C9\u6846\u7EC4\u4EF6",paraId:0},{value:"\u{1F48E} \u57FA\u7840\u4F7F\u7528",paraId:1,tocIndex:2},{value:"\u5C5E\u6027",paraId:2,tocIndex:3},{value:"\u8BF4\u660E",paraId:2,tocIndex:3},{value:"\u7C7B\u578B",paraId:2,tocIndex:3},{value:"\u9ED8\u8BA4\u503C",paraId:2,tocIndex:3},{value:"\u662F\u5426\u5FC5\u4F20",paraId:2,tocIndex:3},{value:"placeholder",paraId:2,tocIndex:3},{value:"\u63D0\u793A\u8BCD",paraId:2,tocIndex:3},{value:"string",paraId:2,tocIndex:3},{value:"\u5426",paraId:2,tocIndex:3},{value:"options",paraId:2,tocIndex:3},{value:"\u4E0B\u62C9\u6846\u7684\u503C",paraId:2,tocIndex:3},{value:"{value:(number string),label:(number string)}[]",paraId:2,tocIndex:3},{value:"[]",paraId:2,tocIndex:3},{value:"\u662F",paraId:2,tocIndex:3}]}}]);
