ed22af44ae009e64ec2ca5085809603070a079bc
Air-xx007 <Air-xx007@users.noreply.github.com>
2025-08-06 11:23:49 +0000

    deploy: e40a98cfbe26ff6feb53d92e69b22d50a3aebb81

---
+++ meta__TextUI.04c41d94.async.js
@@ -0,0 +1,15 @@
+"use strict";(self.webpackChunksic_ui=self.webpackChunksic_ui||[]).push([[3632],{95324:function(c,u,e){e.r(u),e.d(u,{demos:function(){return f}});var i=e(15009),d=e.n(i),I=e(99289),_=e.n(I),a=e(67294),v=e(30613),E=e(11952),f={"textui-demo-0":{component:a.memo(a.lazy(_()(d()().mark(function m(){var l,o,s,r,p;return d()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,Promise.resolve().then(e.bind(e,30613));case 2:return l=n.sent,o=l.Space,n.next=6,Promise.resolve().then(e.bind(e,11952));case 6:return s=n.sent,r=s.TextUI,p=function(){return a.createElement(o,null,a.createElement(r,null,"\u6587\u672C"),a.createElement(r,{type:"primary"},"\u6587\u672C"),a.createElement(r,{type:"danger"},"\u6587\u672C"))},n.abrupt("return",{default:p});case 10:case"end":return n.stop()}},m)})))),asset:{type:"BLOCK",id:"textui-demo-0",refAtomIds:["TextUI"],dependencies:{"index.tsx":{type:"FILE",value:`import { useState } from 'react';
+import { Space } from 'antd';
+import { TextUI } from 'sic-ui';
+
+const App: React.FC = () => {
+  return (
+    <Space>
+      <TextUI>\u6587\u672C</TextUI>
+      <TextUI type="primary">\u6587\u672C</TextUI>
+      <TextUI type="danger">\u6587\u672C</TextUI>
+    </Space>
+  );
+};
+export default App;`},antd:{type:"NPM",value:"5.26.7"},"sic-ui":{type:"NPM",value:"0.8.12"}},entry:"index.tsx"},context:{antd:v,"sic-ui":E},renderOpts:{compile:function(){var m=_()(d()().mark(function o(){var s,r=arguments;return d()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,e.e(250).then(e.bind(e,90250));case 2:return t.abrupt("return",(s=t.sent).default.apply(s,r));case 3:case"end":return t.stop()}},o)}));function l(){return m.apply(this,arguments)}return l}()}}}},69510:function(c,u,e){e.r(u),e.d(u,{texts:function(){return i}});const i=[{value:"\u56FD\u4FE1\u7075\u5DE5\u540E\u53F0\u7BA1\u7406\u7CFB\u7EDF-\u6587\u672C\u7EC4\u4EF6",paraId:0},{value:`\u{1F48E} \u57FA\u7840\u4F7F\u7528
+\u5185\u5BB9\u6837\u5F0F\u4F1A\u505A\u7EE7\u627F\uFF0C\u60AC\u505C\u51FA\u73B0\u5C0F\u624B\u6837\u5F0F`,paraId:1,tocIndex:2},{value:"\u5C5E\u6027",paraId:2,tocIndex:3},{value:"\u8BF4\u660E",paraId:2,tocIndex:3},{value:"\u7C7B\u578B",paraId:2,tocIndex:3},{value:"\u9ED8\u8BA4\u503C",paraId:2,tocIndex:3},{value:"\u662F\u5426\u5FC5\u4F20",paraId:2,tocIndex:3},{value:"type",paraId:2,tocIndex:3},{value:"\u7C7B\u578B",paraId:2,tocIndex:3},{value:"string",paraId:2,tocIndex:3},{value:"default",paraId:2,tocIndex:3},{value:"\u5426",paraId:2,tocIndex:3}]}}]);
